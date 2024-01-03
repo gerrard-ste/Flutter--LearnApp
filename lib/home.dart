bd3cd53d0f28fc47295c0f15144bf093ee66b023
gerrard-ste <127896257+gerrard-ste@users.noreply.github.com>
2024-01-03 17:01:58 +0100

    Hajjouj commit

---
+++ lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:my_application/sign_in.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.png', width: 150, height:150),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Sign_in()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+              ),
+              child: Text('Welcome'),
+            )
+        ]),
+      )
+    );
+  }
+}
---
+++ lib/sign_in.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Sign_in extends StatefulWidget {
+  const Sign_in({super.key});
+
+  @override
+  State<Sign_in> createState() => _Sign_inState();
+}
+
+class _Sign_inState extends State<Sign_in> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In Page'),
+      ),
+      body: Center(
+        child: Text('This is the Sign In page!'),
+      ),
+    );
+  }
+}
